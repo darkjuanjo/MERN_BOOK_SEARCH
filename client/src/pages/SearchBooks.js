2910beaebe348e33234c98658c868e00b32f02f8
Juan Jose Paz <juanjosepazroman@hotmail.com>
2021-12-01 00:59:08 -0800

    fixed typo heroku

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from '@react-bootstrap';
+import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
 import { searchGoogleBooks } from '../utils/API';
