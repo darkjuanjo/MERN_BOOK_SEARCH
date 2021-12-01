af786bea4af86685991e4ad6b127edd086b6b8e3
Juan Jose Paz <juanjosepazroman@hotmail.com>
2021-12-01 01:02:28 -0800

    fixing issues to push to heroku

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
-
+import { Jumbotron, Container, Col, Form, Button, Card} from 'react-bootstrap';
+import CardColumns from '@bit/react-bootstrap.react-bootstrap.card-columns';
 import Auth from '../utils/auth';
 import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
